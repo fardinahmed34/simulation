590231a9d188442320da742f48c8d038f60b59c9
Fardin Ahmed <ahmedfardin107@gmail.com>
2026-09-14 17:22:45 +1000

    Create MatchPhase.java

---
+++ MatchPhase.java
@@ -0,0 +1,15 @@
+package simulation;
+
+public enum MatchPhase {
+    PRE_GAME,
+    KICK_OFF,
+    FIRST_HALF,
+    HALF_TIME,
+    SECOND_HALF,
+    STOPPAGE_TIME,
+    EXTRA_TIME_FIRST_HALF,
+    EXTRA_TIME_SECOND_HALF,
+    PENALTY_SHOOTOUT,
+    POST_GAME,
+    RESULTS
+}
